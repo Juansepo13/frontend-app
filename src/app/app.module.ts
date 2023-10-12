e6be77f1057a0477190fd3ba1348e2ceaa9c98a6
Juansepo13 <juancho_s1314@hotmail.com>
2023-10-12 10:13:10 -0500

    actualizacion por instalacion de angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsModule aquí
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule, // Asegúrate de que FormsModule esté importado aquí
+    FormsModule,
+    BrowserAnimationsModule, // Asegúrate de que FormsModule esté importado aquí
   ],
   providers: [],
   bootstrap: [AppComponent],
